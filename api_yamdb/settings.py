53ca350054cf8a092fa82db0390b967d33857173
PGluk <glukhov.p@gmail.com>
2021-06-11 02:26:48 +0600

    Change database settings

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -55,7 +55,7 @@ WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'postgres',
+        'NAME': os.environ.get('POSTGRES_NAME'),
         'USER': os.environ.get('POSTGRES_USER'),
         'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
